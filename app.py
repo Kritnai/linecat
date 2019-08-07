d554d9e0bae72932a4227a8e7d6dc192bc9dfb16
Kritsanai <51397012+Kritnai@users.noreply.github.com>
2019-08-07 14:21:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,6 @@ def callback():
         sendText(user,'ใช้ภาษาไทยผิดอะป่าว')
     elif (userText == 'ชื่อไรอ่ะ') :
         sendText(user,'ชื่อ Kritnai')
-    elif (userText == 'คนสร้างชื่ออะไร') :
-        sendText(user,'คนสร้างชื่อ นายกริสนัย หอยสังข์')
     else (userText == '') :
         sendText('อาเครๆ')
     return '',200
